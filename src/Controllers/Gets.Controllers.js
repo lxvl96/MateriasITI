4384e803ea178966b5bdbc30b8bc853678be218b
lxvl96 <isclex96@gmail.com>
2020-06-04 20:40:40 -0500

    all

--- src/Controllers/Gets.Controllers.js
+++ src/Controllers/Gets.Controllers.js
@@ -158,6 +158,7 @@ const getSearchmestre = async (req, res, next) => {
                 creditosAcumulados: queryCreditos.rows[0].sum,
                 porcentajeAvance: queryPorcentajeAvance.rows[0].porcentaje,
                 semestreActual: queryMats[queryMats.length - 1].semestre,
+                periodoActual: queryMats[queryMats.length - 1].periodo,
                 materiasInfo: matsData
             })
 
